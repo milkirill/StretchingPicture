d113978242782e4499e5fcf54daac678461083d3
Kirill Milekhin <milkirill@icloud.com>
2023-09-21 16:48:40 +0100

    Solution

--- StretchingPicture/ViewController.swift
+++ StretchingPicture/ViewController.swift
@@ -8,12 +8,87 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    let headerImageView = UIView()
+    let imageView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupScrollView()
+        setupContentView()
+        setupHeader()
+        setupImageView()
     }
-
-
+    
+    func setupScrollView() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        scrollView.delegate = self
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    func setupHeader() {
+        headerImageView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(headerImageView)
+        
+        NSLayoutConstraint.activate([
+            headerImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            headerImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        let headerImageViewBottom : NSLayoutConstraint!
+        headerImageViewBottom = headerImageView.bottomAnchor.constraint(equalTo: contentView.topAnchor, constant: -20)
+        headerImageViewBottom.priority = UILayoutPriority(rawValue: 900)
+        headerImageViewBottom.isActive = true
+    }
+    
+    func setupImageView() {
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "img")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        headerImageView.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: headerImageView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: headerImageView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: headerImageView.bottomAnchor)
+        ])
+        
+        let imageViewTopConstraint = imageView.topAnchor.constraint(equalTo: view.topAnchor)
+        imageViewTopConstraint.priority = UILayoutPriority(rawValue: 900)
+        imageViewTopConstraint.isActive = true
+    }
+    
+    func setupContentView() {
+        contentView.backgroundColor = .systemBackground
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(contentView)
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 270),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: 1500)
+        ])
+    }
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        if offset < 0 {
+            scrollView.verticalScrollIndicatorInsets = UIEdgeInsets(top: 190 - scrollView.contentOffset.y, left: 0, bottom: 0, right: 0)
+        }
+    }
+}
